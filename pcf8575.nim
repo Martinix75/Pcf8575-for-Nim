5d673b1e160c248e3cb9af010cf2aeb5b488f352
Martin Andrea <65339251+Martinix75@users.noreply.github.com>
2025-05-05 16:09:08 +0200

    update pcf8757 ver 2.0.0
    Update for "Pico Stdlib"> = 0.4.0.
    For greater performances "REF object --> " Object ".

--- pcf8575.nim
+++ pcf8575.nim
@@ -8,11 +8,13 @@ author Andrea Martin (Martinix75)
 https://github.com/Martinix75/Pcf8575-for-Nim
 ]#
 
-import picostdlib/[stdio, gpio, time, i2c]
+import picostdlib
+import picostdlib/pico/[stdio, time]
+import picostdlib/hardware/[gpio, i2c]
 from math import log2
 
 const 
-  pcf8575Ver* = "1.0.1" # ripreso dal pfc8574 ad 8 bit.
+  pcf8575Ver* = "2.0.0" # for picoStdLib >= 0.4.0
   p0*: uint16 = 0x0001
   p1*: uint16 = p0 shl 1
   p2*: uint16 = p0 shl 2
@@ -31,32 +33,32 @@ const
   p17*: uint16 = p0 shl 15
 
 type
-  Pcf8575 = ref object #creata the Pcf8575 object.
+  Pcf8575 = object #creata the Pcf8575 object ver 2.0.0 no REF.
     expAddr: uint8
-    blokk: I2cInst
+    blokk: ptr I2cInst
     data: uint16 = 0x0000
-    buffer: array[0..1, byte]
+    buffer: array[0..1, byte] #!! i2c scrive 8 bit (1 byte) alal volta!!
 
-proc makeBuffer(self:Pcf8575) =
+proc makeBuffer(self: var Pcf8575) =
   let byteHigh: byte = byte(self.data shr 8)
   let byteLow: byte = byte(self.data and 0xFF)
   self.buffer[0] = byteLow #write low byte in frist position.
   self.buffer[1] = byteHigh #write high byte in second position.
 
-proc makeData(self: Pcf8575) =
+proc makeData(self: var Pcf8575) =
   self.data = uint16(self.buffer[1]) shl 8 #byte alto si spostadi 8 bit in avti.
   self.data = self.data or self.buffer[0] #byte basso nei primi 8 bit.
 
-proc writeBytes*(self: Pcf8575; data: uint16; inverse: bool = true) =
+proc writeBytes*(self: var Pcf8575; data: uint16; inverse: bool = true) = #modified ver 2.0.0
   if inverse == true: #invereti perche è opencolector ed i numeri  (0=1, e 1=0)
     self.data = not data
   else:
     self.data = data
   self.makeBuffer()
   let addrArray = self.buffer[0].unsafeAddr
-  writeBlocking(self.blokk, self.expAddr, addrArray, 2, false)
+  discard writeBlocking(self.blokk, self.expAddr.I2cAddress, addrArray, 2, false)
   
-proc writeBit*(self: Pcf8575; pin: uint16; value: bool) =
+proc writeBit*(self: var Pcf8575; pin: uint16; value: bool) =
   if value == on:
     self.data = not self.data or pin #data è stata invertita tocca reivertire ogni volta!
     self.writeBytes(data = self.data)
@@ -66,27 +68,56 @@ proc writeBit*(self: Pcf8575; pin: uint16; value: bool) =
       self.data = not self.data xor pin
       self.writeBytes(data = self.data)
 
-proc readBytes*(self: Pcf8575): uint16 =
+proc readBytes*(self: var Pcf8575): uint16 =
   self.buffer[0] = 0; self.buffer[1] = 0
   let addrArray = self.buffer[0].unsafeAddr
-  discard readBlocking(self.blokk, self.expAddr, addrArray, 2, false)
+  discard readBlocking(self.blokk, self.expAddr.I2cAddress, addrArray, 2, false)
   self.makedata()
   result = self.data
   
-proc readBit*(self: Pcf8575; pin: uint16): bool =
+proc readBit*(self: var Pcf8575; pin: uint16): bool =
   let bitValue: uint16 = self.readBytes()
   result = bool(bitValue and pin)
   
   
-proc setLow*(self: Pcf8575) =
+proc setLow*(self: var Pcf8575) =
   self.data = 0x0000
   self.writeBytes(data = self.data)
 
-proc setHigh*(self: Pcf8575) =
+proc setHigh*(self: var Pcf8575) =
   self.data = 0xFFFF
   self.writeBytes(data = self.data)
 
-proc newExpander16*(blokk: I2cInst; expAddr: uint8 = 0x20): Pcf8575 =
+proc initExpander16*(blokk: ptr I2cInst; expAddr: uint8 = 0x20): Pcf8575 =
   result = Pcf8575(blokk: blokk, expAddr: expAddr)
 
+when isMainModule:
+  stdioInitAll()
+  sleepMs(1500)
+  print("Partenza....")
+  var expander = initExpander16(blokk = i2c1, expAddr = 0x20)
+  const 
+    sda = 2.Gpio
+    scl = 3.Gpio
+  discard init(i2c1,50_000)
+  sda.setFunction(I2C); sda.disablePulls()
+  scl.setFunction(I2C); scl.disablePulls()
+  while true:
+    #[print("Spengo tutto...")
+    expander.setLow()
+    sleep(2000)
+    print("Accendo led p2 & p1..")
+    expander.writeBytes(0x0006) #1 byte ttto 1
+    sleep(2500)
+    print("Accendo Led (p2 & p1) + p0")
+    expander.writeBit(p0, on)
+    sleep(2500)
+    print("Spengo P2...")
+    expander.writeBit(p2, off)
+    sleep(2000)]#
+    print("-------------")
+    print("Ora provo a leggere il bit 0......")
+    let lettura = expander.readBit(p0)
+    print("Ho letto: " & $lettura)
+    sleepMs(2000)
 
